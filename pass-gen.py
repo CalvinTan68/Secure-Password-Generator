21523674c38dea451c5128fa3f4df2f88172e0d8
Calvin Tandika <calvintandikaa68@gmail.com>
2022-09-28 13:12:57 +0700

    Upload files

--- pass-gen.py
+++ pass-gen.py
@@ -6,6 +6,8 @@ numbers = '0123456789'
 symbols = '[]{}()*;/.,-_'
 
 combine = smallCase + upperCase + numbers + symbols
-length = input("Digits you need (in numbers) : ")
+print("Welcome to the Secure Password Generator")
+print("Just simply input how many digits you need, and it will auto generate it for you (Max. 75)")
+length = input("Digits you need : ")
 password = "".join(random.sample(combine, int(length)))
 print(password)
