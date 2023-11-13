2883fe203b247a737bab221cf2df4455781ccd1b
Calvin Tandika <74808674+CalvinTan68@users.noreply.github.com>
2023-11-13 20:05:53 +0700

    Add min. digits and minor refactor

--- pass-gen.py
+++ pass-gen.py
@@ -1,10 +1,15 @@
 import random
 import string
 
-chars = string.ascii_letters + string.digits + '!@#$%^&*()_+-=[]{};:<,.">/?`~'
+chars = string.ascii_letters + string.digits + string.punctuation
 
-length = input("Enter the length of the password you need (Max. 75): ")
-length = min(int(length), 75) if length.isdigit() else 10
+while True:
+    length = input("Enter the length of the password you need (between 8 and 75): ")
+    if length.isdigit():
+        length = int(length)
+        if 8 <= length <= 75:
+            break
+    print("Invalid input. Please enter a valid positive integer between 8 and 75.")
 
 password = ''.join(random.choice(chars) for _ in range(length))
 print("Your new password is:", password)
